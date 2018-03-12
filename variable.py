437158aa2763146658075908efe1f1a91fc60ef3
usernamehanillcoer <171wita08@o365.htl-leoben.at>
2018-03-12 14:39:51 +0100

    variable.py umgeschrieben

--- variable.py
+++ variable.py
@@ -1,1 +1,35 @@
+# variable
 
+a = 10
+b = 20
+ summe = a + b
+
+ print("Summe von", a, " + ", b, " = ", summe)
+
+aktion = input("Welche Aktion willt du? (+/-/*/:)")
+print("Aktion ", aktion, " wird ausgrführt...")
+
+if(aktion == "*")
+    print("Multiplikation ausgewählt.")
+    a*b
+if(aktion == "+")
+    print("plus ausgewählt.")
+    a+b
+if(aktion == "-")
+    print("Minus ausgewählt.")
+    a-b
+    if(aktion == ":")
+        print("division ausgewählt.")
+        a/b
+         
+zahl = input("Welche Zahl wählst du?")
+zahl = int(zahl)
+
+zahl2 = int(23)
+
+zahlA = "123"
+zahlB = "456"
+print(zahlA+zahlB)
+#Ausgae: 123456, weil es sich hier um einen STRING handelt!
+
+zahlC = 987
