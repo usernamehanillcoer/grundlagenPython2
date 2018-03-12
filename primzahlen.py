2dd746bea35cb708c2a879a742b28c53c09113d6
usernamehanillcoer <171wita08@o365.htl-leoben.at>
2018-03-12 16:16:53 +0100

    primzahlen

--- primzahlen.py
+++ primzahlen.py
@@ -1,3 +1,25 @@
 # prime-number
 # Funktion: check if given number is prime
-# wetter heute sonnig bis bewölkt
+
+def isPrime(n):
+    #Logik einbauen
+    # return True
+    if(n <= 1):
+        return False
+
+#gibt es eine Division ohne REST?
+#dann ist es keiine Primzahl
+    for p in range(2,n):
+        if(n % p == 0):
+            return False
+
+
+    return True
+
+print("Primzahlen-Checker")
+z = input("Bitte die Zahl eingeben: ")
+z = int(z)
+if (isPrime(z)):
+    print(z, "ist eine Primzahl")
+else:
+    print(z, "ist KEINE Primzahl")
